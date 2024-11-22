1505950c5393b2934f9b892df47e89a748d1d9c7
Scott Yinan Fan <scottvan2008@gmail.com>
2024-11-23 06:02:09 +0800

    Initial commit

---
+++ Assignment_3_skeleton/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_skeleton
+{
+	public class Node
+	{
+		//field
+		object element;
+		Node successor;
+		//setter and getter
+		public object Element { get => element; set => element = value; }
+		public Node Successor { get => successor; set => successor = value; }
+		//constructor with one parameter
+		public Node(object o)
+		{
+			Element = o;
+		}
+		//constructor with two parameters
+		public Node(object o, Node n)
+		{
+			Element = o;
+			Successor = n;
+		}
+	}
+}
